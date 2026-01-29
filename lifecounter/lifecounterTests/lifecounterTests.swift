a452e1d24462d5c58d067c6696742c69f2118330
David <davidvt@uw.edu>
2026-01-28 22:22:20 -0800

    create project

---
+++ lifecounter/lifecounterTests/lifecounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  lifecounterTests.swift
+//  lifecounterTests
+//
+//  Created by iguest on 1/28/26.
+//
+
+import Testing
+@testable import lifecounter
+
+struct lifecounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
